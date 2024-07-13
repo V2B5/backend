ec4f060113293908e730ee4f4283b0da5f63241f
V2B5 <117782333+V2B5@users.noreply.github.com>
2024-07-13 19:18:05 +0100

    novoBackend

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '50mb' }));
